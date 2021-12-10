b7e4f0a901445fbd4177afd7c7202a55ab27938a
nasonova22 <nasonova_ga@mail.ru>
2021-12-11 00:50:54 +0300

    lesson1

---
+++ HomeWorkLesson1.java
@@ -0,0 +1,33 @@
+public class HomeWorkLesson1 {
+    public static void main (String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    static void printThreeWords () {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    static void checkSumSign () {
+        int a = -40;
+        int b = -20;
+		System.out.println(a+b >= 0?"Sum positive":"Sum negative");
+    }
+	static void printColor (){
+        int a = 110;
+        if (a <= 0) { System.out.println("Red");
+        }
+        if ((a > 0) & (a <= 100)) { System.out.println("Yellow");
+        }
+        if (a > 100) { System.out.println("Green");
+        }
+    }
+    static void compareNumbers (){
+        int a, b;
+        a = 80;
+        b = 50;
+        System.out.println(a >= b?"a >= b":"a<b");
+    }
+}
